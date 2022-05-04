d8a651c6e74dfdbf03ace9ef4a703e6e40dd688b
Anderson Lobo <andersonalobo@hotmail.com>
2022-05-03 23:47:57 -0300

    Added Webapi configurations and dockerfile.

--- src/Gcsb.Connect.Training/Gcsb.Connect.Training.Webapi/Program.cs
+++ src/Gcsb.Connect.Training/Gcsb.Connect.Training.Webapi/Program.cs
@@ -1,25 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using Autofac.Extensions.DependencyInjection;
+using Gcsb.Connect.Training.Webapi;
+using Serilog;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+startup.Configure(app, app.Environment);
 
 app.Run();
---
+++ src/Gcsb.Connect.Training/Gcsb.Connect.Training.Webapi/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Autofac;
+using Autofac.Configuration;
+using Autofac.Extensions.DependencyInjection;
+using Gcsb.Connect.Pkg.Startup.Webapi.DependencyInjection;
+using Gcsb.Connect.Pkg.Startup.Webapi.Resources;
+using Gcsb.Connect.Training.Webapi.DependencyInjection;
+using Gcsb.Connect.Training.Webapi.Pipeline;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Localization;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+
+namespace Gcsb.Connect.Training.Webapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public ILifetimeScope AutofacContainer { get; private set; }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ConfigurationModule(Configuration));
+            builder.AddAutofacRegistration();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllersWithViews();
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c => 
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Treinamento", Version = "v1" });
+                });
+            services.AddLocalization();
+            services.AddVersioning();
+            services.AddProblemDetails();
+            services.AddCustomFilters();
+            services.Cors();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.MapControllers();
+            IServiceProvider serviceProvider = app.Services.GetRequiredService<IServiceProvider>();
+            var resources = serviceProvider.GetService<IStringLocalizer<ReturnMessages>>();
+            this.AutofacContainer = serviceProvider.GetAutofacRoot();
+
+            app.UseExceptionHandler(new ExceptionHandlerOptions
+            {
+                ExceptionHandler = new ErrorHandlerMiddleware(environment, resources).Invoke
+            });
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            app.AddLocalization();
+            app.UseCors();
+            app.UseProblemDetails();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(e => e.MapControllers());
+            app.AddOptions();
+        }
+    }
+}
